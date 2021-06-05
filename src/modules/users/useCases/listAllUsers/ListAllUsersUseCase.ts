2e02282650ce141e94e815421c8897e72260e8d6
Vitor Mendes <vitor.mendes93@gmail.com>
2021-06-05 17:04:39 -0300

    desafio concluido

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,17 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
     // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user || !user.admin) {
+      throw new Error("User dosen`t exists or is not an admin");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
